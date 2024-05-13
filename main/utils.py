5fedad88adaf3072a5cd13ce2ab214410491a46c
lokes <lokeswargahir349@gmail.com>
2024-05-14 01:38:28 +0530

    changed ip setting

--- main/utils.py
+++ main/utils.py
@@ -3,10 +3,10 @@ from socket import gethostname, gethostbyname
 def get_ip(request):
     # host = gethostname()
     # return gethostbyname(host)
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    # ip = request.META.get('REMOTE_ADDR')
+    # x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    # if x_forwarded_for:
+    #     ip = x_forwarded_for.split(',')[-1].strip()
+    # else:
+    #     ip = request.META.get('REMOTE_ADDR')
+    ip = request.META.get('REMOTE_ADDR')
     return ip
