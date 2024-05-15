b262057a9eebb11a5b06f169b1c23a4f23f9203f
lokes <lokeswargahir349@gmail.com>
2024-05-15 15:57:56 +0530

    printing x_forward_for IP

--- main/utils.py
+++ main/utils.py
@@ -4,6 +4,7 @@ def get_ip(request):
     # host = gethostname()
     # return gethostbyname(host)
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    print('x_forwarded_for:', x_forwarded_for)
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0].strip()
     else:
